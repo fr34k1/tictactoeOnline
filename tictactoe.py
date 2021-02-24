343f01c56a0fb208745cb5e52c12e8c4d8a8d1e3
freaky one <76263495+fr34k1@users.noreply.github.com>
2021-02-24 13:44:42 -0300

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -54,7 +54,7 @@ class Tictactoe:
 
 
 
-
+'''
 	def play():
 
 
@@ -85,27 +85,5 @@ class Tictactoe:
 
 
 
-
-
-class Player:
-
-
-	def __init__(self,name):
-		self.name = name
-		self.movement=None
-
-	def name(self):
-		return self.name
-
-	def doPlay(self,movements):
-
-		while True:
-			self.movement = input("[Game Client] Do your movement\n")
-			if self.movement in movements:
-				break
-			
-			print("[Game Client] Entrada invalida")
-		return self.movement
-
-
+'''
 
